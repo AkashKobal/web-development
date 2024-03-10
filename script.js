ffc99d17173fbf41b4fed292a1b97f2f2d6c2d86
Akash <akashkobal02@gmail.com>
2024-03-11 00:37:02 +0530

    update deployment

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let mainFrame = document.getElementById("main-frame");
+
+function btn1(){
+    mainFrame.src = "1.%20Calculator/index.html";
+
+}
